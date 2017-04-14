f80dcc125acb02a353cdc5740f442897cc17fa2c
Dotunj <dotunjolaosho@gmail.com>
2017-04-15 00:02:03 +0100

    fixed app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,11 @@
 $(document).foundation()
-	$(".success button").click(function(){
-		$("#homerow2").show();
-	});
+
+var questions = [{
+    number: 1,
+    question:"Please choose an appropriate sex:",
+    choices:["Male", "Female", "Bobrisky"],
+}, {
+    number: 2,
+    question:"What do you do for a living?",
+    choices:["Developer", "Designer", "Content Provider","Banker","Teacher","Doctor",,"Lawyer","Taxify/Uber Driver","Taxify/Uber Driver", "Intern at a startup in Yaba","Staying woke on twitter", "Alariwo", "I am a Nigerian Politician"],
+}];
