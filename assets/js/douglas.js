9337949f45392a5b392da67412939b23bee78410
Douglas Kesi-Ayeba Kendyson <kendyson@kendyson.com>
2017-04-16 00:43:37 +0100

    Next button

--- assets/js/douglas.js
+++ assets/js/douglas.js
@@ -1,31 +1,43 @@
 var answers = [];
-      var currentQuestion = 0;
-      var quizOver = false; 
+  var currentQuestion = 0;
+  var quizOver = false; 
 
-      function displayCurrentQuestion() {
+  function displayCurrentQuestion() {
 
-          console.log("In display current Question");
+      console.log("In display current Question");
 
-          var question = questions[currentQuestion].question;
-          var question_no = questions[currentQuestion].number;
-          var numChoices = questions[currentQuestion].choices.length;
+      var question = questions[currentQuestion].question;
+      var question_no = questions[currentQuestion].number;
+      var numChoices = questions[currentQuestion].choices.length;
 
 
-          $('.question_no').html(question_no+"/"+numChoices);
-          $('.question').text(question);
+      $('.question_no').html(question_no+"/"+numChoices);
+      $('.question').text(question);
 
-          $('.options').html('<label class="options_label"></label>');
+      $('.options').html('<label class="options_label"></label>');
 
-          var choice;
-          for (i = 0; i < numChoices; i++) {
-              choice = questions[currentQuestion].choices[i];
-              $('<input type="radio" value=' + i + ' name="'+question_no+'" /><label for="' + i + '"><p id="pcolor">' + choice + '</p></label><br>').appendTo('.options_label');
-          }
+      var choice;
+      for (i = 0; i < numChoices; i++) {
+          choice = questions[currentQuestion].choices[i];
+          $('<input type="radio" value=' + i + ' name="'+question_no+'" /><label for="' + i + '"><p id="pcolor">' + choice + '</p></label><br>').appendTo('.options_label');
       }
+  }
 
-      $(document).ready(function () {
-        displayCurrentQuestion()
-      });
+$(document).ready(function () {
+
+  displayCurrentQuestion();
+
+   $(".next_button").on("click", function () {
+
+     currentQuestion++;
+
+     displayCurrentQuestion();
+
+   });
+
+
+
+});
 
 
 
